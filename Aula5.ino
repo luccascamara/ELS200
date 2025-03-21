09c000e9aa354db7b6f08b738a231193e079d108
Lais <laislmorais06@gmail.com>
2025-03-21 08:57:37 -0300

    Update Aula5.ino

--- Aula5.ino
+++ Aula5.ino
@@ -1,6 +1,3 @@
-//incompleto - falta ajustar nas curvas
-
-
 #include <AFMotor.h>
 
 AF_DCMotor motor1(1); 
@@ -8,13 +5,11 @@ AF_DCMotor motor2(2);
 AF_DCMotor motor3(3); 
 AF_DCMotor motor4(4);
 
-int tempo2 = 2000;
 int S1 = 25;
 int S2 = 27;
 int S3 = 29;
 int S4 = 31;
 int S5 = 33;
-bool start = false;
 
 int S1_lido = 0;
 int S2_lido = 0;
@@ -24,15 +19,20 @@ int S5_lido = 0;
 
 void moverFrente();
 void moverTras();   
-void virar(int tempo);  
+void virar();  
 void parar(); 
 
 void setup() {
-  motor1.setSpeed(154); 
-  motor2.setSpeed(154); 
-  motor3.setSpeed(154); 
-  motor4.setSpeed(154); 
-  delay(1000);
+  motor1.setSpeed(105); 
+  motor2.setSpeed(105); 
+  motor3.setSpeed(105); 
+  motor4.setSpeed(105); 
+
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(S4, INPUT);
+  pinMode(S5, INPUT);
 }
 
 void loop() {
@@ -42,33 +42,21 @@ void loop() {
   S4_lido = digitalRead(S4);
   S5_lido = digitalRead(S5);
 
-  if (S3_lido == LOW && S2_lido == LOW && S4_lido == LOW) {
-    start = true;
+
+  if(S3_lido == LOW) {
     moverFrente();
   }
-  
-  if (S3_lido == LOW) {         
-    moverFrente();        
+
+  else if(S1_lido == LOW || S2_lido == LOW || S4_lido == LOW || S5_lido == LOW) {
+    virar();
   }
 
-  if (S1_lido == LOW && S2_lido == LOW && S3_lido == LOW && S4_lido == LOW && S5_lido == LOW) {
-    moverTras();
-    delay(1000); 
-    moverFrente(); 
+  else {
+    parar(); 
   }
-
-  if (S3_lido == LOW && S2_lido == LOW && S4_lido == LOW && start == true) {
-    start = false;
-    parar();
-  }
-}  
+}
 
 void moverFrente() {  
-  motor1.setSpeed(120); 
-  motor2.setSpeed(120); 
-  motor3.setSpeed(120); 
-  motor4.setSpeed(120); 
-  
   motor1.run(FORWARD); 
   motor3.run(FORWARD); 
   motor2.run(BACKWARD); 
@@ -76,34 +64,22 @@ void moverFrente() {
 }
 
 void moverTras() {  
-  motor1.setSpeed(120); 
-  motor2.setSpeed(120); 
-  motor3.setSpeed(120); 
-  motor4.setSpeed(120); 
-  
   motor1.run(BACKWARD); 
   motor3.run(BACKWARD); 
   motor2.run(FORWARD); 
   motor4.run(FORWARD);
 }
 
-void virar(int tempo2) {
-  motor1.setSpeed(130); 
-  motor2.setSpeed(150); 
-  motor3.setSpeed(150); 
-  motor4.setSpeed(130); 
-  
+void virar() {
   motor1.run(FORWARD); 
   motor3.run(BACKWARD); 
   motor2.run(BACKWARD); 
   motor4.run(FORWARD);
-  delay(tempo2);
 }
 
-void parar() {
+void parar(){
   motor1.run(RELEASE); 
   motor3.run(RELEASE); 
   motor2.run(RELEASE); 
   motor4.run(RELEASE);
-  delay(500); 
 }
